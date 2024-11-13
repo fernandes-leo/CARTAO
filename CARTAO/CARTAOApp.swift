540f9fe82bd780957fc2977ac281b05905cd126d
Leonardo <fernandes.leo@live.com>
2024-11-12 23:16:41 -0300

    Initial Commit

---
+++ CARTAO/CARTAOApp.swift
@@ -0,0 +1,17 @@
+//
+//  CARTAOApp.swift
+//  CARTAO
+//
+//  Created by Leonardo Fernandes on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CARTAOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
