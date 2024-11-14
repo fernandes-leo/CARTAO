82a5678044bfef5a16ecafed26d8ec1e3d7d8e70
Leonardo <fernandes.leo@live.com>
2024-11-14 16:37:45 -0300

    CARTAO

---
+++ CARTAO/FrontOfCard.swift
@@ -0,0 +1,18 @@
+//
+//  FrontOfCard.swift
+//  CARTAO
+//
+//  Created by Leonardo Fernandes on 12/11/24.
+//
+
+import SwiftUI
+
+struct FrontOfCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrontOfCard()
+}
